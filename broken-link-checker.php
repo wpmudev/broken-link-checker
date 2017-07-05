9975a53f8e1efa6df970f7887a631e5a4a44056e
Carlos Longarela <carlos@longarela.eu>
2017-07-05 13:34:48 +0200

    Updated plugin name

--- broken-link-checker.php
+++ broken-link-checker.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Broken Link Checker
+Plugin Name: Broken Link Checker Fork
 Plugin URI: http://w-shadow.com/blog/2007/08/05/broken-link-checker-for-wordpress/
-Description: Checks your blog for broken links and missing images and notifies you on the dashboard if any are found.
-Version: 1.11.2
+Description: Checks your blog for broken links and missing images and notifies you on the dashboard if any are found. This is a fork version based in 1.11.2 verion
+Version: 1.0.0
 Author: Janis Elsts, Vladimir Prelovac
 Author URI: http://w-shadow.com/
 Text Domain: broken-link-checker
