573081bd0a0440fb77a103bbbc8858a03d1f934e
Zrgisa <sgrivcov@godaddy.com>
2017-08-01 18:32:13 +0200

    include 1.11.3 changes

--- broken-link-checker.php
+++ broken-link-checker.php
@@ -1,23 +1,22 @@
 <?php
 /*
 Plugin Name: Broken Link Checker
-Plugin URI: http://w-shadow.com/blog/2007/08/05/broken-link-checker-for-wordpress/
+Plugin URI: https://wordpress.org/plugins/broken-link-checker/
 Description: Checks your blog for broken links and missing images and notifies you on the dashboard if any are found.
-Version: 1.11.2
+Version: 1.11.3
 Author: Janis Elsts, Vladimir Prelovac
-Author URI: http://w-shadow.com/
 Text Domain: broken-link-checker
 */
 
-// Path to this file.
-if ( ! defined( 'BLC_PLUGIN_FILE' ) ) {
-	define( 'BLC_PLUGIN_FILE', __FILE__ );
+//Path to this file
+if ( !defined('BLC_PLUGIN_FILE') ){
+	define('BLC_PLUGIN_FILE', __FILE__);
 }
 
-// Path to the plugin's directory.
-if ( ! defined( 'BLC_DIRECTORY' ) ) {
-	define( 'BLC_DIRECTORY', dirname( __FILE__ ) );
+//Path to the plugin's directory
+if ( !defined('BLC_DIRECTORY') ){
+	define('BLC_DIRECTORY', dirname(__FILE__));
 }
 
-// Load the actual plugin.
+//Load the actual plugin
 require 'core/init.php';
