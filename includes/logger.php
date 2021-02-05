48381754e00dd9f83f5c1ea57a88441014533d0d
Matt Geri <mattgeri@gmail.com>
2021-02-05 14:56:45 +0200

    Re-add Mike hotfix

--- includes/logger.php
+++ includes/logger.php
@@ -114,7 +114,7 @@ if ( ! class_exists( 'blcLogger' ) ) :
 		}
 
 		function save() {
-			update_option( $this->option_name, $this->log );
+			update_option( $this->option_name, $this->log, false );
 		}
 	}
 
